30a91c280e586873689fe866102b1d75fd193447
Thi Quach <amylyquach@gmail.com>
2025-07-02 15:22:33 +1000

    JSX with JS expression inside HTML

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+var fname = "Amy";
+var lname = "Q";
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1>
+      Hello {fname} {lname}
+    </h1>
+    <p>My lucky number is {Math.floor(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 );
