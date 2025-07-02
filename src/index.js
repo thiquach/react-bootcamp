cd77bbc66d7e3296df09d6080a33854e518543a3
Thi Quach <amylyquach@gmail.com>
2025-07-02 16:43:06 +1000

    Inline styling usage

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-var picture = "https://picsum.photos/200";
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black",
+};
+
+customStyle.color = "blue";
+
 ReactDOM.render(
   <div>
-    <h1 className="heading">My Favourite Foods</h1>
-    <div>
-      <img
-        className="food-img"
-        src="https://upload.wikimedia.org/wikipedia/commons/3/31/Made20bacon.png"
-        alt="bacon"
-      />
-      <img
-        className="food-img"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShmnkZ-4VclOBc-kHc-sFyjHeD5vZSV0urqA&s"
-        alt="jamon"
-      />
-      <img
-        className="food-img"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXiCh9QdvSiq7g5SRR1_74NaADakwY8s6VAYa19vNCns0h9uQHADxVuZVmvO2V06c8OGM&usqp=CAU"
-        alt="noodles"
-      />
-    </div>
-    <h1>My Favourite Picture</h1>
-    <img src={picture + "?grayscale"} alt="random" />
+    <h1 style={customStyle}>Hello World! - inline styling on the fly</h1>
+    <p>How inline styling is used</p>
   </div>,
   document.getElementById("root")
 );
