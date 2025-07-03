21ad3054d08882bd25729bd7f2d64b591c7da60e
Thi Quach <amylyquach@gmail.com>
2025-07-03 12:30:57 +1000

    Styling on the fly practice

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const date = new Date(2025, 1, 1, 19);
+const hours = date.getHours();
+let greeting;
+
 const customStyle = {
-  color: "red",
-  fontSize: "20px",
-  border: "1px solid black",
+  color: "",
 };
 
-customStyle.color = "blue";
+if (hours < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (hours > 12 && hours < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "blue";
+} else {
+  greeting = "Good Evening";
+  customStyle.color = "green";
+}
 
 ReactDOM.render(
   <div>
-    <h1 style={customStyle}>Hello World! - inline styling on the fly</h1>
-    <p>How inline styling is used</p>
+    <h1 className="heading" style={customStyle}>
+      {greeting}
+    </h1>
+    <p>Practice styling</p>
   </div>,
   document.getElementById("root")
 );
