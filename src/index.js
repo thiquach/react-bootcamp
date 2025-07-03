ae318b7ccc884a02e554845627f388e7d26b6069
Thi Quach <amylyquach@gmail.com>
2025-07-03 13:32:33 +1000

    react components introduction

--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const date = new Date(2025, 1, 1, 19);
-const hours = date.getHours();
-let greeting;
-
-const customStyle = {
-  color: "",
-};
-
-if (hours < 12) {
-  greeting = "Good Morning";
-  customStyle.color = "green";
-} else if (hours < 18) {
-  greeting = "Good Afternoon";
-  customStyle.color = "blue";
-} else {
-  greeting = "Good Evening";
-  customStyle.color = "red";
-}
-
-ReactDOM.render(
-  <div>
-    <h1 className="heading" style={customStyle}>
-      {greeting}
-    </h1>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
