924f30d9840c4033f3cdbec00d17ef87be642e3e
Thi Quach <amylyquach@gmail.com>
2025-07-04 10:08:18 +1000

    ES6 import export practice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import PI, { doublePi, triplePi } from "./math.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ul>
+    <li>{PI}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
