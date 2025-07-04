8e86ccb79ec4a2febb9664c96478d0f0f562b660
Thi Quach <amylyquach@gmail.com>
2025-07-04 11:28:23 +1000

    add keeper app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as Calculator from "./calculator.js";
+import App from "./components/App";
 
-ReactDOM.render(
-  <ul>
-    <li>{Calculator.add(1, 2)}</li>
-    <li>{Calculator.multiply(2, 3)}</li>
-    <li>{Calculator.subtract(7, 2)}</li>
-    <li>{Calculator.divide(5, 2)}</li>
-  </ul>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
