f3319052f218ed8f778668b0d8132c351d840c11
Thi Quach <amylyquach@gmail.com>
2025-07-04 12:15:08 +1000

    react prop introduction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,30 @@
 import React from "react";
-import Header from "./Header";
-import Note from "./Note";
-import Footer from "./Footer";
+import Card from "./Card";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Note />
-      <Footer />
+      <h1>My Contacts</h1>
+      <Card
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+123 456 789"
+        email="b@beyonce.com"
+      />
+      <Card
+        name="Jack Bauer"
+        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        alt="avatar_img"
+        tel="+987 654 321"
+        email="jack@nowhere.com"
+      />
+      <Card
+        name="Chuck Norris"
+        img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        alt="avatar_img"
+        tel="+918 372 574"
+        email="gmail@chucknorris.com"
+      />
     </div>
   );
 }
