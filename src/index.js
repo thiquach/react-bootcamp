320a65b91cf5f5644ea2e16b3155c304884b2709
Thi Quach <amylyquach@gmail.com>
2025-07-06 16:18:53 +1000

    map(), substring() to cut meaning to 100

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-var numbers = [3, 56, 2, 48, 5];
+import emojipedia from "./emojipedia";
 
-//Map -Create a new array by doing something with each item in an array.
-const newNumbers1 = numbers.map((x) => x * 2);
-console.log(newNumbers1);
-
-//Filter - Create a new array by keeping the items that return true.
-const newNumbers2 = numbers.filter((x) => x > 10);
-console.log(newNumbers2);
-
-//Reduce - Accumulate a value by doing something to each item in an array.
-const newNumbers3 = numbers.reduce(function reduce(accumulator, currentNum) {
-  return accumulator + currentNum;
+const newDescription = emojipedia.map(function shorten(emojiEntry) {
+  return emojiEntry.meaning.substring(0, 100);
 });
-console.log(newNumbers3);
-
-//Find - find the first item that matches from an array.
-const newNumbers4 = numbers.find((x) => x > 10);
-console.log(newNumbers4);
-
-//FindIndex - find the index of the first item that matches.
-const newNumbers5 = numbers.findIndex((x) => x > 10);
-console.log(newNumbers5);
+console.log(newDescription);
