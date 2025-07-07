c9856e7a9e4a5675c7cda0e806888f5f5e278053
Thi Quach <amylyquach@gmail.com>
2025-07-07 16:28:19 +1000

    keeper app 2 starting point

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,14 @@
 import React from "react";
-import Entry from "./Entry";
-import emojipedia from "../emojipedia";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
-      <dl className="dictionary">
-        {emojipedia.map((emojiTerm) => (
-          <Entry
-            key={emojiTerm.id}
-            emoji={emojiTerm.emoji}
-            name={emojiTerm.name}
-            description={emojiTerm.meaning}
-          />
-        ))}
-      </dl>
+      <Header />
+      <Note />
+      <Footer />
     </div>
   );
 }
