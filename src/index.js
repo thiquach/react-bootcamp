3d28fcdeada7f751f2cc5b5b13a8dfde29c0e12d
Thi Quach <amylyquach@gmail.com>
2025-07-07 15:41:43 +1000

    change to arrow functions

--- src/index.js
+++ src/index.js
@@ -5,29 +5,31 @@ import "./../public/styles.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
 
-////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+//Map -Create a new array by doing something with each item in an array.
+const newNumbers1 = numbers.map((x) => x * x);
 
-//////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+console.log(newNumbers1);
+
+//Filter - Create a new array by keeping the items that return true.
+const newNumbers2 = numbers.filter((num) => num < 10);
+
+console.log(newNumbers2);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+var newNumber3 = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
 
-////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+console.log(newNumber3);
 
-////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+//Find - find the first item that matches from an array.
+const newNumber4 = numbers.find((num) => num > 10);
+
+console.log(newNumber4);
+
+//FindIndex - find the index of the first item that matches.
+const newNumber5 = numbers.findIndex((num) => num > 10);
+
+console.log(newNumber5);
