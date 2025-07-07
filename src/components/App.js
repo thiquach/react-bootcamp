e3f3a3ec03e5715d25070c5d8c2d1aec1635cc23
Thi Quach <amylyquach@gmail.com>
2025-07-07 17:04:03 +1000

    keeper app 2 starting point

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./Header";
-import Note from "./Note";
 import Footer from "./Footer";
+import Note from "./Note";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
       <Footer />
+      <Note />
     </div>
   );
 }
