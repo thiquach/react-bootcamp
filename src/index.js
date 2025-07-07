a041d540ffaa12bced3de3407803925671f14bc8
Thi Quach <amylyquach@gmail.com>
2025-07-07 15:27:57 +1000

    code before arrow funcion practice

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-import emojipedia from "./emojipedia";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./../public/styles.css";
 
-const newDescription = emojipedia.map(function shorten(emojiEntry) {
-  return emojiEntry.meaning.substring(0, 100);
-});
-console.log(newDescription);
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// var numbers = [3, 56, 2, 48, 5];
+
+////Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map(function (x) {
+//   return x * 2;
+// });
+
+//////Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter(function(num) {
+//   return num < 10;
+// });
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+//     return accumulator + currentNumber;
+// })
+
+////Find - find the first item that matches from an array.
+// const newNumber = numbers.find(function (num) {
+//   return num > 10;
+// })
+
+////FindIndex - find the index of the first item that matches.
+// const newNumber = numbers.findIndex(function (num) {
+//   return num > 10;
+// })
