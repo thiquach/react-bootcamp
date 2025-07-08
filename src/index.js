22b6fb55cbc3968e6a166ab8278904e9c780b387
Thi Quach <amylyquach@gmail.com>
2025-07-08 11:36:59 +1000

    conditional rendering starting point

--- src/index.js
+++ src/index.js
@@ -4,6 +4,3 @@ import App from "./components/App";
 import "./../public/styles.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
