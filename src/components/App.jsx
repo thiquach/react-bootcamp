f7f48b24dea926ec6aa2d4ccf13ffe3f566ce20d
Thi Quach <amylyquach@gmail.com>
2025-07-08 16:01:28 +1000

    useState hooks practice starting point

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function increase() {
-    setCount(count + 1);
-  }
-
-  function decrease() {
-    setCount(count - 1);
-  }
   return (
     <div className="container">
-      <h1>{count}</h1>
-      <button onClick={increase}>+</button>
-      <button onClick={decrease}>-</button>
+      <h1>TIME</h1>
+      <button>Get Time</button>
     </div>
   );
 }
