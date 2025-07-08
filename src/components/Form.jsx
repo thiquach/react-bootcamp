1da0d83cb6f65b3d7db22a5e2ecea536f328a407
Thi Quach <amylyquach@gmail.com>
2025-07-08 12:49:50 +1000

    show confim password button if user is not register

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,9 @@ function Form(props) {
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
+      {!props.isRegister && (
+        <input type="password" placeholder="Confirm Password" />
+      )}
       <button type="submit">{props.isRegister ? "Login" : "Register"}</button>
     </form>
   );
