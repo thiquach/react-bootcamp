be07a78c65f2d73737af4525322a41c315f3d325
Thi Quach <amylyquach@gmail.com>
2025-07-08 15:33:31 +1000

    add minus button to decrease count using state hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,10 +6,15 @@ function App() {
   function increase() {
     setCount(count + 1);
   }
+
+  function decrease() {
+    setCount(count - 1);
+  }
   return (
     <div className="container">
       <h1>{count}</h1>
       <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
