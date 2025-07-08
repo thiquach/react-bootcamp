81c45a81518ecdf8e9529443f3140c76ad011211
Thi Quach <amylyquach@gmail.com>
2025-07-08 15:21:42 +1000

    add useState hook update count when clicking

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
   return (
     <div className="container">
-      <h1>0</h1>
-      <button>+</button>
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
     </div>
   );
 }
