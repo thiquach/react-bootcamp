1de4242074e600a88806754d2e6f75a98b205d94
Thi Quach <amylyquach@gmail.com>
2025-07-09 15:39:28 +1000

    add on-click mouse-over  mouse-out event handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
