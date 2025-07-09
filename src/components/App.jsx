49e29c07f9b90fd194602beb886fb0b1ecf063a5
Thi Quach <amylyquach@gmail.com>
2025-07-09 17:00:25 +1000

    changing complex state starting point

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-  const [name, setName] = useState("");
-  const [headingText, setHeadingText] = useState("");
-
-  function handleChange(event) {
-    setName(event.target.value);
-  }
-
-  function handleClick(event) {
-    setHeadingText(name);
-
-    event.preventDefault();
-  }
-
   return (
     <div className="container">
-      <h1>Hello {headingText}</h1>
-      <form onSubmit={handleClick}>
-        <input
-          onChange={handleChange}
-          type="text"
-          placeholder="What's your name?"
-          value={name}
-        />
-        <button type="submit">Submit</button>
+      <h1>Hello</h1>
+      <form>
+        <input name="fName" placeholder="First Name" />
+        <input name="lName" placeholder="Last Name" />
+        <button>Submit</button>
       </form>
     </div>
   );
