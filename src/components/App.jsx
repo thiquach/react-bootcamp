618c5b5cbd2a0a00dc897a41069d849af3cfe1e2
Thi Quach <amylyquach@gmail.com>
2025-07-09 16:24:03 +1000

    add on-click on-submit form event handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setHeadingText(name);
+
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
