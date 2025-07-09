b122f7794f909fadd892877f5fc80e8dfdf9e833
Thi Quach <amylyquach@gmail.com>
2025-07-09 16:05:54 +1000

    complex event handling in react form starting point

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-  const [headingText, setHeadingText] = useState("Hello");
-  const [isMouseOver, setMouseOver] = useState(false);
-
-  function handleClick() {
-    setHeadingText("Submitted");
-  }
-
-  function handleMouseOver() {
-    setMouseOver(true);
-  }
-
-  function handleMouseOut() {
-    setMouseOver(false);
-  }
-
   return (
     <div className="container">
-      <h1>{headingText}</h1>
+      <h1>Hello </h1>
       <input type="text" placeholder="What's your name?" />
-      <button
-        onClick={handleClick}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
-      >
-        Submit
-      </button>
+      <button>Submit</button>
     </div>
   );
 }
