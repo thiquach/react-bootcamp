e2760b88570e5da8b8492341d2eb69a4a08e89cc
Thi Quach <amylyquach@gmail.com>
2025-07-09 17:28:01 +1000

    first part of changing complex state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+
+  function handleFname(event) {
+    const newFname = event.target.value;
+
+    setFname(newFname);
+  }
+
+  function handleLname(event) {
+    const newLname = event.target.value;
+
+    setLname(newLname);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fname} {lname}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={handleFname}
+          name="fName"
+          placeholder="First Name"
+          value={fname}
+        />
+        <input
+          onChange={handleLname}
+          name="lName"
+          placeholder="Last Name"
+          value={lname}
+        />
         <button>Submit</button>
       </form>
     </div>
