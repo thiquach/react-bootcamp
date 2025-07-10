33a8bb35ed6448ab87852b925dee7fd769f4aeed
Thi Quach <amylyquach@gmail.com>
2025-07-10 12:10:17 +1000

    Changing complex state practice starting point

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,22 @@
 import React, { useState } from "react";
 
 function App() {
-  const [fullName, setFullName] = useState({
+  const [contact, setContact] = useState({
     fName: "",
     lName: "",
+    email: ""
   });
-
-  function handleChange(event) {
-    const { value, name } = event.target;
-
-    // no directly reference event.target in setFullName()
-    // as it is syntheticEvent
-    setFullName((prevValue) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-        };
-      } else if (name == "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-        };
-      }
-    });
-  }
 
   return (
     <div className="container">
       <h1>
-        Hello {fullName.fName} {fullName.lName}
+        Hello {contact.fName} {contact.lName}
       </h1>
+      <p>{contact.email}</p>
       <form>
-        <input
-          onChange={handleChange}
-          name="fName"
-          placeholder="First Name"
-          value={fullName.fName}
-        />
-        <input
-          onChange={handleChange}
-          name="lName"
-          placeholder="Last Name"
-          value={fullName.lName}
-        />
+        <input name="fName" placeholder="First Name" />
+        <input name="lName" placeholder="Last Name" />
+        <input name="email" placeholder="Email" />
         <button>Submit</button>
       </form>
     </div>
